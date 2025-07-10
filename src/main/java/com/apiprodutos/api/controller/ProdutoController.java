cae0a533c342c5f379af445b0d1d728635621c84
death-angel2 <vitorpedrocalixto@gmail.com>
2025-07-09 21:29:53 -0300

    v2

--- src/main/java/com/apiprodutos/api/controller/ProdutoController.java
+++ src/main/java/com/apiprodutos/api/controller/ProdutoController.java
@@ -1,10 +1,15 @@
 package com.apiprodutos.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.apiprodutos.api.models.Produto;
 import com.apiprodutos.api.models.ProdutoRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 
@@ -14,15 +19,23 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository repository;
 
-    @GetMapping("/listagem")
-    public String listarprodutos(){
+    @GetMapping("/")
+    public String listarProdutos(Model model){
+    model.addAttribute("todosOsProdutos", repository.findAll());
         return "listar";
     }
 
     @GetMapping("/cadastro")
-    public String cadastrarprodutos(){
+    public String mostrarFormulario(Model model){
+        model.addAttribute("produto", new Produto());
         return "cadastrar";
+  
     }
 
+    @PostMapping("/cadastro")
+    public String cadastrarProdutos(Produto produto){
+        repository.save(produto);
+        return "cadastrar";
 
+}
 }
